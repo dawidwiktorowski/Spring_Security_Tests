ffdc22eb3cc552d3e5abce55019b35873fd3e2ce
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-12 08:40:40 +0100

    Added complete RegistrationRequest

---
+++ src/main/java/com/dawidwiktorowski/spring_security_tests/registaration/RegistrationRequest.java
@@ -0,0 +1,17 @@
+package com.dawidwiktorowski.spring_security_tests.registaration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+}
