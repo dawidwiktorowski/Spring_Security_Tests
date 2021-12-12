e20df2fbd629f87b4c960bfea666108babe456ba
dawidwiktorowski <dawid.wiktorowski22@gmail.com>
2021-12-12 08:36:59 +0100

    Added password encoder

---
+++ src/main/java/com/dawidwiktorowski/spring_security_tests/security/PasswordEncoder.java
@@ -0,0 +1,14 @@
+package com.dawidwiktorowski.spring_security_tests.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
